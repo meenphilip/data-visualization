7b5cef488f802ee3c9b5bef9a76b32c8df01d454
Meen Philip <meenphilip@gmail.com>
2023-07-30 20:36:12 +0300

    Median Python Developer Salaries by Age

--- mpl_demo.py
+++ mpl_demo.py
@@ -2,12 +2,18 @@ from matplotlib import pyplot as plt
 
 # Median Developer Salaries by Age
 # x - axis
-dev_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
+ages_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
 
 # y - axis
 dev_y = [38496, 42000, 46752, 49320, 53200, 56000, 62316, 64928, 67317, 68748, 73752]
 
-plt.plot(dev_x, dev_y)
+plt.plot(ages_x, dev_y)
+
+# Median Python Developer Salaries by Age
+ages_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
+py_dev_y = [45372, 48876, 53850, 57287, 63016, 65998, 70003, 70000, 71496, 75370, 83640]
+
+plt.plot(ages_x, py_dev_y)
 
 # Add labels.
 plt.xlabel("Ages")
